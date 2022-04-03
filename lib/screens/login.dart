4a950cf7a33aa749a03df198de3e3462e554ad7b
Anjala <ianjala@yahoo.com>
2022-04-03 14:51:50 +0530

    created file structure and login screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({ Key? key }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Gym App',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15,),
+              Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () {
+                      print(nameController.text);
+                      print(passwordController.text);
+                    },
+                  )
+              ),
+              // TextButton(
+              //   onPressed: () {
+              //     //forgot password screen
+              //   },
+              //   child: const Text('Forgot Password',),
+              // ),
+              Row(
+                children: <Widget>[
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      //signup screen
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ],
+          )),
+    );
+  }
+}
